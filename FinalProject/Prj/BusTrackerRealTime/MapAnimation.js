5b510f96adedc1c6695d407f7f3e9adf822c1dc0
Gopibkk <gopinath900@hotmail.com>
2021-07-05 21:37:26 +0700

    t

---
+++ FinalProject/Prj/BusTrackerRealTime/MapAnimation.js
@@ -0,0 +1,73 @@
+// define our access token
+const accessToken = 'pk.eyJ1IjoicnN3ZWV0ZW4wMSIsImEiOiJja3BwZ2I2anEyNzhsMnBueW5sdG5zcno2In0.6uWMmZrrC9prUL-2AI0kmg';
+
+// set the access token
+mapboxgl.accessToken = accessToken;
+
+// create our map
+var map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/streets-v11',
+    center: [-71.091542,42.358862],
+    zoom: 12
+});
+
+// define an empty array for markers for right now
+const markers = [];
+
+// function to asynchronously request bus data from MBTA
+async function getBusData(){
+	const url = 'https://api-v3.mbta.com/vehicles?filter[route]=1&include=trip';
+	const response = await fetch(url);
+	const json     = await response.json();
+	return json.data;
+}
+
+// define an object to describe a particular bus
+function Bus(id, coordinates) {
+    this.id = id;
+    this.coordinates = coordinates;
+    return this;
+}
+// utility function to shortcut extracting from JSON
+Bus.fromJSON = function (json) {
+    return new Bus(json.id, getBusCoordinates(json))
+}
+
+// retrieve coordinate data from the parsed JSON
+function getBusCoordinates(bus) {
+    return [bus.attributes.longitude, bus.attributes.latitude];
+}
+
+// define an update() function to handle data retrieval and update markers
+async function update(map, markers = []) {
+    let busData = await getBusData();
+    let busses = busData.map((bus) => Bus.fromJSON(bus));
+
+    // loop through the busses
+    for (bus of busses) {
+        // if a marker doesn't exist for this bus, add one
+        let currentMarker = markers.find((item) => item.id == bus.id);
+        if (!currentMarker) {
+            markers.push({
+                id: bus.id,
+                marker: new mapboxgl.Marker()
+                .setLngLat(bus.coordinates)
+                .addTo(map)
+            });
+        }
+        // if one does, update it
+        else {
+            currentMarker.marker.setLngLat(bus.coordinates);
+        }
+    }
+
+    // [TODO]: handle busses leaving service
+}
+
+// run update() once to get initial data
+update(map, markers);
+// set update() to run every 15 seconds, save the timer ID
+let intervalId = setInterval(() => update(map, markers), 15000);
+// tell the window to cancel the timer when the window closes
+window.onunload = () => clearInterval(intervalId);
---
+++ FinalProject/Prj/BusTrackerRealTime/README.md
@@ -0,0 +1,17 @@
+## Real Time Bus Tracker
+
+ This is a Bus Tracker in real time to map out the buses that travel along the route from MIT to Harvard. The mapping uses the MBTA API for data on the Buses latest location update then supplies that information to mapboxgl to display the locations on the map.
+ 
+ Once the MapAnimations.js, styles.css, and index.html are copied the bus tracker will work without issue.
+
+## Future Improvements: 
+Add more routes
+
+Add in options to highlight routes 
+
+## MIT License copyright <2021> <Robert Sweeten>
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
